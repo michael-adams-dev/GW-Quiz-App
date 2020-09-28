61fb945f02a6a5ff16fe2c18f40d03724a88ec42
Michael Adams <michaeladams914.dev@gmail.com>
2020-09-28 16:59:37 -0700

    Initial commit: add gems to gemfile and created greeting class

---
+++ Gemfile
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem "open-uri", "~> 0.1.0"
+
+gem "nokogiri", "~> 1.10"
+
+gem "tty-prompt", "~> 0.22.0"
+
+gem "down", "~> 5.2"
+
+gem "rtesseract", "~> 3.1"
+
+gem "byebug", "~> 11.1"
+
+gem "terminal-table", "~> 1.8"
+
+gem "rubocop", "~> 0.92.0"
---
+++ lib/greeting.rb
@@ -0,0 +1,26 @@
+require_relative 'quiz_helper'
+require_relative 'picuki_doc'
+
+class Greeting
+  include QuizHelper
+  
+  attr_reader :facts
+  
+  def initialize
+    @facts = FACTS
+  end
+
+  def greet
+    puts "Welcome to the Good Weekend Quiz App!"
+    puts "Here you can download the Good Weekend quiz from the last two weeks."
+    puts "Or you can select a prior quiz from the quiz database."
+    puts "Please wait a moment..."
+    puts ""
+  end
+
+  def random_fact
+    fact_num = rand(0..1)
+    puts "Did you know? #{FACTS[fact_num]}"
+    puts ""
+  end
+end
