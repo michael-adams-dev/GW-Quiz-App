1634ab8659337aa78b73c8f6be2136766cd5454d
Michael Adams <michaeladams914.dev@gmail.com>
2020-09-30 20:30:59 -0700

    Added comments to prep_answers

--- lib/prep_answers.rb
+++ lib/prep_answers.rb
@@ -1,11 +1,3 @@
-ans1 = "QUIZ ANSWERS\n\n1 Darwin.\n2. Paper (parchment or grease-proof; used to cover the surface of a stock,\nstew, sauce or soup to reduce evaporation).\n3 East River.\n4 Battle (invasion) of Normandy (launched on June 6, 1944, D-Day).\n5 Fashion.\n6 “Thrilla in Manila”.\n7 On an aircraft (attached to the outboard trailing edge of each wing,\nused to control a plane when rolling or banking).\n\n8 Sarah Snook.\n\n9 Wurundjeri.\n\n10 Lady Gaga.\n\n11 A Cavalier King Charles spaniel and a poodle.\n12 Broadcaster video-on-demand.\n\n13 Korean Air.\n\n14 Bob Hawke.\n15 Sea snail.\n\n16 Heights.\n17 Ten-pin bowling (three strikes in a row).\n18 Germany, Switzerland and Austria.\n19 Plimsoll line.\n20 Groove Armada.\n21 Snagglepuss (cartoon character created by Hanna-Barbera).\n22 Fruit. 23 The speed of light.\n24 Coronation.\n25 Canberra (ACT) and Griffith (NSW).\n\nGET IT? Halfback.\n\f"
-
-
-ans2 = "QUIZ ANSWERS\n\n1 Women's wheelchair basketball.\n2 5 per cent (7.692 million sq km).\n3 Argentina (derived from the Latin argentum, meaning silver).\n4 Elizabeth Debicki.\n5 The Birthday Party.\n6 European rivers.\n7 String (the bow frog is the end part of a stringed musical instrument's bow).\n8 Russell Crowe.\n9 Dawn.\n10 High-definition multimedia interface.\n11 Lieutenant Colonel Bill Kilgore (played by Robert Duvall) in Apocalypse Now.\n12 Lonely Street.\n13 125 (5x5x5).\n14 Azerbaijan.\n15 Jurassic (201 million to 145 million years ago).\n16 Bayern Munich (defeating Paris Saint-Germain 1-0).\n17 Ontology.\n18 Three (NSW, SA and NT).\n19 Mrs Beeton (Isabella Beeton).\n20 Hannah Gadsby.\n21 Sichuan province.\n22 Seven.\n23 Fencing.\n24 Whitechapel (in London's East End).\n25 Dame Mary Gilmore\n(featured on the $10 note) was Scott Morrison’s great-great aunt.\n\nGET IT? Listen.\n\f"
-
-
-ans3 = "QUIZ ANSWERS\n\n1 $US1 million.\n2 Aleister Crowley (1875-1947).\n3 Dodecagonal (12-sided).\n4 Triple point.\n5 Medal of the Order.\n6A foot.\n7 The 1st Academy Awards ceremony.\n\n8 The stomach.\n9 The Yield.\n\n10 The Mediterranean and the Red seas.\n11 Sabre-toothed tiger or sabre-toothed cat.\n12 The Netherlands.\n\n13 Peru and Brazil.\n\n14 /'m a Believer (composed by Neil Diamond, first recorded by The Monkees in 1966).\n15 Sternum.\n\n16 Lion (red).\n\n17 Boxer Muhammad Ali.\n\n18 Arrabbiata.\n\n19 Australia.\n\n20 Pitch Perfect.\n\n21 String.\n\n22 Game of Thrones.\n\n23 Sound.\n\n24 United Arab Emirates.\n\n25 Campanologist (bell ringer).\n\nGET IT? Monochrome.\n\f"
-
 class String
   def numeric?
     Float(self) != nil rescue false
@@ -17,9 +9,11 @@ def remove_characters(str)
   str.split(' ')
 end
 
+# Answer strings will sometimes be formatted with a full stop after
+# the answer number. This needs to be removed for answers to be separated
 def correct_formatting(ans_str)
   formatted = [] 
-  ans_str.each_with_index do | str, index |
+  ans_str.each_with_index do |str, index|
     if str.end_with?('.') && ans_str[index - 1].end_with?('.')
       formatted << str.chop
     else
@@ -29,6 +23,12 @@ def correct_formatting(ans_str)
   formatted
 end
 
+# Now we need to segment the array into each distinct answer.
+# Sub strings will be added to a new array (single_answer) until
+# the substring ends with a full stop, which indicates that is
+# the end of the answer.
+# Single_answer is then added to arr_of_strings, then cleared 
+# So it can start gathering the sub strings of the next answer.
 def answer_split(split_strings)
   arr_of_strings = []
   single_answer = []
@@ -45,42 +45,59 @@ def answer_split(split_strings)
   arr_of_strings
 end
 
-def question_join(arr)
-  strings_joined = arr.map do |question|
-    question.join(' ')
+# Now we need to join the sub strings that form each answer
+# so loop through the array to get each array of sub strings
+# then join the sub stings together with a space
+# finally, remove the quiz answers string from the start of 
+# answer 1
+def answer_join(arr)
+  strings_joined = arr.map do |answer|
+    answer.join(' ')
   end
   strings_joined[0] = strings_joined[0].gsub("QUIZ ANSWERS ", '')
   strings_joined
 end
 
+# Now we need to sort the answers in numeric order so the question
+# and answer orders match. Start by splitting each answer into two
+# so we have a string with the answer number and one with the answer
+# There should only be 25 answers so until there is that amount, remove
+# the last.
+# finally, convert numbers to integers to be sorted and string whitespace
+# from the answers.
 def sort_answers(arr)
-  ans_arr = arr.map do |question|
-    question.split(' ', 2)
+  ans_arr = arr.map do |answer|
+    answer.split(' ', 2)
   end
   until ans_arr.count <= 25
     ans_arr.pop
   end
-  q_num_to_i = ans_arr.map do |question|
-    question = [question[0].to_i, question[1].strip]
+  a_num_to_i = ans_arr.map do |answer|
+    answer = [answer[0].to_i, answer[1].strip]
   end
-  return q_num_to_i.sort
+  return a_num_to_i.sort
 end
 
-def arr_to_hash(questions_arr)
+# The last step is to turn the question or answer array into a hash
+# The method creates a blank has and maps the array integers to the key
+# and the array string (Q or A) to the value and returns the hash
+def arr_to_hash(q_or_a_arr)
   questions_hash = {}
-  questions_arr.each do |question|
-    questions_hash[question.first] = question.last
+  q_or_a_arr.each do |q_or_a|
+    questions_hash[q_or_a.first] = q_or_a.last
   end
   questions_hash
 end
 
-def a_string_to_hash(ans1)
-  test1 = remove_characters(ans1)
-  test2 = correct_formatting(test1)
-  test3 = answer_split(test2)
-  test4 = question_join(test3)
-  test5 = sort_answers(test4)
-  arr_to_hash(test5)
+# This method combines the previous answer manipulation methods from
+# to finish
+def a_string_to_hash(str)
+  step1 = remove_characters(str)
+  step2 = correct_formatting(step1)
+  step3 = answer_split(step2)
+  step4 = answer_join(step3)
+  step5 = sort_answers(step4)
+  arr_to_hash(step5)
 end
 
 test = a_string_to_hash(ans2)
