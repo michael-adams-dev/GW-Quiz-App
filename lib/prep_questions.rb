12c74addaae3a2575453dd73eb54d1c9ed0bcb50
Michael Adams <michaeladams914.dev@gmail.com>
2020-09-30 19:39:08 -0700

    Created methods to convert quiz questions string to hash

---
+++ lib/prep_questions.rb
@@ -0,0 +1,81 @@
+str1 = "THE QUIZ\n\nCOMPILED BY Facqui Martinez & Stephanie Bull\n\n \n\n1. Palmerston was the\nformer name of which\nAustralian city?\n\n2. What is a cartouche, used\nin French cooking, made from?\n\n3. New York City’s\nQueensboro Bridge crosses\nwhich river?\n\n4. Operation Overlord\nwas the codename for\nwhich battle during\nWorld War II?\n\n5. Australia’s Indigenous\nNIFA awards celebrate\nexcellence in what industry?\n\n6. What tagline was given to\nthe boxing match between Joe\nFrazier and Muhammad Ali in\nManila in 1975?\n\n7. Where would you\nfind an aileron?\n\n8. Which Australian actress\nplays Shiv Roy in the TV series\nSuccession?\n\n9. Who are the traditional\nowners of greater\nMelbourne: Wurundjeri,\nGadigal or Larrakia?\n\n10. Which artist won the inaugural\n\nTricon award at the 2020 MTV\nVideo Music Awards?\n\n18. Which three\nEuropean countries\nshare Lake Constance?\n\n19. Starting with “P”,\nwhat name is given to\nthe load limit line on\na ship’s hull?\n\n \n\n11. A cavoodle is a cross between\nwhich two dog breeds?\n\n12. What does the initialism\n\n“BVOD\" stand for?\n\n13. The logo of which\ninternational airline is based\non the yin and yang symbol?\n\n14. “The Silver Bodgie”\nwas a nickname for which\nformer Australian PM?\n\n15. What is another name for\n\na whelk?\n\n16. What is acrophobia the\n\nfear of?\n\n17. In which sport can you\n\nbowl a turkey?\n\n20. Edge of the Horizon\nis which electronic band's\nfirst album in 10 years?\n\n21. Which pink\nanthropomorphic\nmountain lion aspired\nto be a stage actor?\n\n22. Is an olive a fruit,\na vegetable or a nut?\n\n23. What does “c” represent\nin the equation E = mc2?\n\n24. What is the name of artist Ai\nWeiwei's 2020 coronavirus film?\n\n25. Walter Burley and\nMarion Mahony Griffin\nhelped design which\ntwo Australian cities?\n\f"
+
+str2 = "THE QUIZ\n\nCOMPILED BY Facqui Martinez & Stephanie Bull\n\n \n\n1. The name of which\neconomic system is derived\nfrom the French for “let it be”?\n\n2. Velma Dinkley is a\nbespectacled fictional\ndetective in which franchise?\n\n3. What does cadence\nmeasure in cycling?\n\n10. What shade of colour\nis carmine?\n\n18. Arame, hijiki and\nkombu are types of ... what?\n\n \n\n11. The name of which\nEastern European country\nmeans “White Russia”?\n\n12. Who had an “excellent\n\nadventure”, a “bogus journey”\nand now “face the music”?\n\n \n\n4. Nine members of which\ncountry’s royal family were\nmassacred in 2001?\n\n5. Which two planets\nin the solar system have no\nmoons?\n\n13. The Rosetta Stone is\nhoused in which museum?\n\n19. What are Thwaites,\nPerito Moreno and Fox?\n\n20. Who did actor Ed\nDevereaux play in the TV\nseries Skippy?\n\n21. What was the first\ncurrency to be minted\nin Australia?\n\n \n\n14. Hitler's 1934 purge of the\nSA (Brownshirts) by the SS\nwas known as the Night of the\nLong ... what?\n\n \n\n6. Which Scottish lake has the\nlargest volume of fresh water in\nGreat Britain?\n\n7. What is your axilla more\n\ncommonly known as?\n\n8. Which Beatles song begins:\n\n“You say yes, | say no\"?\n\n15. Name the athlete who\nwrote Able: Gold Medals,\nGrand Slams and Smashing\nGlass Ceilings.\n\n16. In the House of\nRepresentatives, what object\nsymbolises the authority of the\nSpeaker?\n\n \n\n9. What is traditionally\nadded to a martini to\nmake it “dirty”?\n\n17. What are the key\ningredients in spaghetti\naglio e olio?\n\n22. What breed of cat\nis also the name of a\nmotorcycle grand prix?\n\n23. In genetics,\nwhat do the initials\nDNA stand for?\n\n24. Who will be Jacinda\nArdern's political nemesis at\nNZ's election in October?\n\n25. “Twas brillig,\n\nand the slithy toves”\n\nis the first line of which\nfamous 19th-century\npoem?\n\n \n\f"
+
+
+str3 = "THE QUIZ\n\nCOMPILED BY Facqui Martinez & Stephanie Bull\n\n \n\n1. In which sport\ndo the Australian\nGliders compete?\n\n2. Australia makes up what\n\nper cent of the world’s land\nmass: 5, 10, or 15?\n\n3. Which South American\n\ncountry is named after\nan element?\n\n4. Which Australian actor will\n\nplay Princess Diana in the final\ntwo seasons of The Crown?\n\n9. In Roman mythology, Aurora\nis the goddess of ... what?\n\n10. What do the initials\n“HDMI” stand for?\n\n11. Who said, “I love the\nsmell of napalm in the\nmorning” in a 1979 film?\n\n12. On what street is Elvis\nPresley's Heartbreak Hotel?\n\n13. What is the value\nof 5 cubed?\n\n18. With how many states and\nterritories does Queensland\nshare a land border?\n\n19. Who wrote The Book\nof Household Management\nin 1861?\n\n20. Which Australian\ncomedian owns a Lagotto\nRomagnolo named Douglas?\n\n21. Mapo tofu is a\npopular dish from which\nChinese province?\n\n \n\n5. What is the name of\nAmerican rapper Kanye\nWest’s political party for\nhis presidential bid?\n\n6. What are the Elbe, the\n\nOder and the Vistula?\n\n7. In which section of\nan orchestra would you\nfind a “frog”?\n\n8. For whom did Sotheby's\n\nAustralia hold an “Art of\nDivorce” auction in 2018?\n\n14. Baku is the capital city\nof which country?\n\n15. In which geologic period\n\ndid birds evolve: Triassic,\nJurassic or Cretaceous?\n\n22. How many justices\ncomprise a full bench in\nAustralia's High Court?\n\n23. In which sport would\nyou wear a lamé?\n\n \n\n16. Which soccer team\nrecently won the 2020\nUEFA Champions League?\n\n24. Which district of London\nis associated with the serial\nkiller Jack the Ripper?\n\n \n\n17. Starting with “O”,\n\nwhich branch of\nmetaphysics deals with the\nnature of being?\n\n25. What links the\nAustralian $10 note\nand Prime Minister\nScott Morrison?\n\f"
+
+
+class String
+  def numeric?
+    Float(self) != nil rescue false
+  end
+end
+
+def remove_characters(str)
+  str = str.gsub(/\n/, ' ').gsub(/\f/, ' ').gsub(/\s+/, ' ')
+  return str.split(' ')
+end
+
+def question_split(split_strings)
+  arr_of_strings = []
+  single_question = []
+  split_strings.each do |str|
+    if str.length == 3 || str.length == 2
+      if str[0].numeric? && str[-1] == '.'
+        arr_of_strings << single_question
+        single_question = []
+        single_question << str
+      else
+        single_question << str
+      end
+    else
+      single_question << str
+    end
+  end
+  arr_of_strings << single_question
+  arr_of_strings
+end
+
+def question_join(arr)
+  strings_joined = arr.map do |question|
+    question.join(' ')
+  end
+  p strings_joined
+  strings_joined
+end
+
+def sort_questions(arr)
+  q_arr = arr.map do |question|
+    question.split('.', 2)
+  end
+  q_arr.shift
+  q_num_to_i = q_arr.map do |question|
+    question = [question[0].to_i, question[1].strip]
+  end
+  return q_num_to_i.sort
+end
+
+def arr_to_hash(questions_arr)
+  questions_hash = {}
+  questions_arr.each do |question|
+    questions_hash[question.first] = question.last
+  end
+  questions_hash
+end
+
+def q_string_to_hash(str)
+  test1 = remove_characters(str)
+  test2 = question_split(test1)
+  test3 = question_join(test2)
+  test4 = sort_questions(test3)
+  arr_to_hash(test4)
+end
+
+test = q_string_to_hash(str3)
+
+test.each do |number, question|
+  puts "Question #{number} #{question}"
+end
+
+
