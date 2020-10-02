a0036c3ddcd4fd162fb1a82dcb737c881c7edd9d
Michael Adams <michaeladams914.dev@gmail.com>
2020-10-02 15:48:32 -0700

    Added read and write quiz methods to quiz_db

--- lib/quiz_db.rb
+++ lib/quiz_db.rb
@@ -1,8 +1,11 @@
 require 'json'
 require_relative 'quiz_helper'
+require_relative 'quiz'
 
 class QuizDB
-include QuizHelper
+  include QuizHelper
+
+  attr_accessor :quizzes
 
   def initialize
     @quizzes = read_quizzes
@@ -12,8 +15,27 @@ include QuizHelper
     data = File.read(QUIZ_DATABASE)
     JSON.parse(data).map do |quiz|
       Quiz.new(
-        quiz
+        quiz['questions'],
+        quiz['actual_answers'],
+        quiz['id'],
+        quiz['date'],
+        quiz['user_answers'],
+        quiz['score']
       )
     end
+  end
+
+  def write_quizzes
+    data = @quizzes.map do |quiz|
+      {
+        questions: quiz.questions,
+        actual_answers: quiz.actual_answers,
+        id: quiz.id,
+        date: quiz.date,
+        user_answers: quiz.user_answers,
+        score: quiz.score
+      }
+    end
+    File.write(QUIZ_DATABASE, JSON.pretty_generate(data))
   end
 end
